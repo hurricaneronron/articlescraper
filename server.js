cfb6c1b3043a07fde188d26036bf720aee7cb8df
hurricaneronron <vprzybyl@gmail.com>
2018-05-25 13:15:49 -0700

    set up server/add hbs/models

---
+++ server.js
@@ -0,0 +1,69 @@
+// npm packages
+var express = require('express')
+var bodyparser = require('body-parser')
+var path = require('path')
+var morgan = require('morgan')
+var expresshbs = require('express-handlebars')
+var mongoose = require("mongoose")
+var cheerio = require("cheerio")
+var request = require("request")
+
+// new express app
+var app = express()
+
+// middleware
+app.use(morgan('dev'))
+app.engine('hbs', expresshbs({defaultLayout: 'main', extname: '.hbs'}))
+app.set('view engine', 'hbs')
+app.use(express.static(path.join(__dirname, '/public/')))
+app.use(bodyparser.urlencoded({ extended: true }))
+app.use(bodyparser.json())
+
+// routes
+app.get('/', function (req, res) {
+  res.render('index.hbs')
+})
+
+// A GET route for scraping the echoJS website
+app.get("/scrape", function(req, res) {
+  // First, we grab the body of the html with request
+  axios.get("https://www.nytimes.com/").then(function(response) {
+    // Then, we load that into cheerio and save it to $ for a shorthand selector
+    var $ = cheerio.load(response.data);
+
+    // Now, we grab every h2 within an article tag, and do the following:
+    $("article h2").each(function(i, element) {
+      // Save an empty result object
+      var result = {};
+
+      // Add the text and href of every link, and save them as properties of the result object
+      result.title = $(this)
+        .children("a")
+        .text();
+      result.link = $(this)
+        .children("a")
+        .attr("href");
+
+      // Create a new Article using the `result` object built from scraping
+      db.Article.create(result)
+        .then(function(dbArticle) {
+          // View the added result in the console
+          console.log(dbArticle);
+        })
+        .catch(function(err) {
+          // If an error occurred, send it to the client
+          return res.json(err);
+        });
+    });
+
+    // If we were able to successfully scrape and save an Article, send a message to the client
+    res.send("Scrape Complete")
+  })
+})
+
+// listening port
+var PORT = process.env.PORT || 3000
+app.listen(PORT, function (e) {
+  if (e) throw e
+  console.log('Listening on PORT ' + PORT)
+})
